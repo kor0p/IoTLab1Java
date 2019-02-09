8f2b1dbd98b333f8f018ad3f13611ea18e08d9da
Віктор Коропецький <32600554+kor0p@users.noreply.github.com>
2019-02-10 00:11:21 +0200

    Create class XmasTree
    Create class "Artificial Christmas tree", that have fields:
    - name of production company
    - height (in centimeters)
    - price
    - material of production
    
    Two extra fields, that best way describes class:
        radius of basis, country of production
    Methods to set and get this fields
    Static field (number type)
    Add two protected fields
    Three constructors
    (default, with first 4 fields/parameters
    and with all fields/parameters)
    Methods toString, (static method) printStatic<Field>, print<Field>, resetValues

---
+++ XmasTree.java
@@ -0,0 +1,120 @@
+package com.lab1.xmastree;
+
+public class XmasTree {
+	private String name = "NoName";
+	private int height = 0;
+	private int price = 0;
+	private String material = "NoMaterial"; // material of production
+	
+	private int radius = 0; // radius of basis
+	private String country = "NoCountry"; // country of production
+	
+	private static int numberOfTrees = 0;
+	
+	protected int numberOfBranches = 0;
+	protected boolean wasUsed = false;
+	
+	public XmasTree()
+	{
+		numberOfTrees++;
+	}
+	public XmasTree (String name, int height, int price, String material)
+	{
+		this();
+		this.name = name;
+		this.height = height;
+		this.price = price;
+		this.material = material;
+	}
+	public XmasTree (
+			String name, int height, int price, String material,
+			int radius, String country, int numberOfBranches, boolean wasUsed
+			)
+	{
+		this(name, height, price, material);
+		this.radius = radius;
+		this.country = country;
+		this.numberOfBranches = numberOfBranches;
+		this.wasUsed = wasUsed;
+	}
+	
+	public void setName(String name)
+	{	this.name = name;}
+	public String getName()
+	{	return name;}
+	
+	public void setHeight(int height)
+	{	this.height = height;}
+	public int getHeight()
+	{	return height;}
+	
+	public void setPrice(int price)
+	{	this.price = price;}
+	public int getPrice()
+	{	return price;}
+	
+	public void setMaterial(String material)
+	{	this.material = material;}
+	public String getMaterial()
+	{	return material;}
+	
+	public void setRadius(int radius)
+	{	this.radius = radius;}
+	public int getRadius()
+	{	return radius;}
+	
+	public void setCountry(String country)
+	{	this.country = country;}
+	public String getCountry()
+	{	return country;}
+	
+	public String toString()
+	{
+		return    " name: "+name+
+			"\n height: "+height+"cm"+
+			"\n price: "+price+"$"+
+			"\n material: "+material+
+			"\n radius: "+radius+"cm"+
+			"\n country: "+country+
+			"\n num of branches: "+numberOfBranches+
+			"\n was used: "+wasUsed+
+			"\n";
+	}
+	public static void printStaticNumberOfTrees()
+	{
+		System.out.println("number of trees: "+numberOfTrees);
+	}
+	public void printNumberOfTrees()
+	{
+		System.out.println("number of trees: "+numberOfTrees);
+	}
+	public void resetValues(
+			String name, int height, int price, String material,
+			int radius, String country, int numberOfBranches, boolean wasUsed
+			)
+	{
+		this.name = name;
+		this.height = height;
+		this.price = price;
+		this.material = material;
+		this.radius = radius;
+		this.country = country;
+		this.numberOfBranches = numberOfBranches;
+		this.wasUsed = wasUsed;
+	}
+	
+	public static void main(String[] args)
+	{
+		XmasTree tree1 = new XmasTree();
+		XmasTree tree2 = new XmasTree(
+				"Christmas feast", 150, 200,"plastic");
+		XmasTree tree3 = new XmasTree(
+				"XmasHen", 300, 400, "plastic+metal",
+				100, "Ukraine", 100, true);
+		System.out.println(tree1);
+		System.out.println(tree2);
+		System.out.println(tree3);
+		XmasTree.printStaticNumberOfTrees();
+		tree1.printNumberOfTrees();
+	}
+}
